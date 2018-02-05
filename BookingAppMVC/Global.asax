e388c6c0edeaf6bb0d294581ca0a217839a8c9ee
douglas1410 <dpham@kth.se>
2018-02-05 17:07:52 +0100

    Add project files.

---
+++ BookingAppMVC/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="BookingAppMVC.MvcApplication" Language="C#" %>
---
+++ BookingAppMVC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(BookingAppMVC.Startup))]
+namespace BookingAppMVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
